aa2411df761069952e2817d205cbbd695acc8774
asmuro1 <f.aurelien@yahoo.fr>
2023-10-31 19:58:19 +0100

    redirection si pas de pass définit en local

--- games/urls.py
+++ games/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
+
+
+def local_redirect(request):
+    return redirect("math_index")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("mathador/", include("mathador.urls"))
+    path("mathador/", include("mathador.urls")),
+    path("", local_redirect)
 ]
--- mathador/urls.py
+++ mathador/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from mathador import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name="math_index")
 ]
