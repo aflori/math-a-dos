a018f6f9c361a0fe334613738660df8ba1540514
asmuro1 <f.aurelien@yahoo.fr>
2023-11-01 18:14:31 +0100

    create game form now create a new game

--- mathador/views.py
+++ mathador/views.py
@@ -5,8 +5,8 @@ from mathador.DTO.repository.game_repository_in_DB import GameRepositoryInDB
 from mathador.models import Board
 from mathador.form import CreateGameForm
 
-
 # Create your views here.
+game_repo = GameRepositoryInDB()
 
 
 def index(request):
@@ -15,7 +15,6 @@ def index(request):
                    "form": CreateGameForm()}
         return render(request, "mathador/index.html", context)
     else:
-        game_repo = GameRepositoryInDB()
         return http.HttpResponse(game_repo.get_all_game())
 
 
@@ -23,5 +22,8 @@ def create_game(request):
     if request.method == "POST":
         form = CreateGameForm(request.POST)
         if form.is_valid():
+            from mathador.action.create_game.create_game_command import CreateGame
+            create_command = CreateGame(game_repo)
+            create_command.execute(form.cleaned_data["number_of_case"], form.cleaned_data["number_of_player"])
             return redirect("math:math_index")
     raise http.Http404()
