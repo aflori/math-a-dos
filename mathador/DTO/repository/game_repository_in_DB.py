52e6a7bf98e2b9291d1ab0a5cdcde65a57a528d9
asmuro1 <f.aurelien@yahoo.fr>
2023-11-01 18:13:52 +0100

    implemented save() method on database repository

--- mathador/DTO/repository/game_repository_in_DB.py
+++ mathador/DTO/repository/game_repository_in_DB.py
@@ -1,11 +1,34 @@
-from mathador.DTO.board import Board
-from mathador.DTO.case import Case
-from mathador.DTO.dice import Dice
-from mathador.DTO.player import Player
+from mathador.DTO.board import Board as DTO_Board
+from mathador.DTO.case import Case as DTO_Case
+from mathador.DTO.dice import Dice as DTO_Dice
+from mathador.DTO.player import Player as DTO_Player
+
+from mathador.models import Board as DB_Board
+from mathador.models import Case as DB_Case
+from mathador.models import Dice as DB_Dice
+from mathador.models import Player as DB_Player
 
 
 def _extract_dice_from_model_object(dice):
-    return Dice(dice.number_of_face, dice.last_number_throws, dice.id)
+    return DTO_Dice(dice.number_of_face, dice.last_number_throws, dice.id)
+
+
+def _save_dice(db_board, dto_dice, result_dice=False, movement_dice=False):
+    db_dice = DB_Dice(from_board=db_board, last_number_throws=dto_dice.last_number_throw,
+                      number_of_face=dto_dice.number_of_face, is_result_dice=result_dice,
+                      is_movement_dice=movement_dice)
+    if dto_dice.id != 0:
+        db_dice.id = dto_dice.id
+    db_dice.save()
+
+
+def _save_case(db_board, dto_case):
+    db_case = DB_Case(from_board=db_board, case_number=dto_case.case_number_on_board,
+                      mandatory_operation=dto_case.mandatory_operation,
+                      optional_operation=dto_case.optional_operation)
+    if dto_case.id != 0:
+        db_case.id = dto_case.id
+    db_case.save()
 
 
 class GameRepositoryInDB:
@@ -30,19 +53,46 @@ class GameRepositoryInDB:
 
         # case var
         cases = self._case_DB.filter(from_board=board).order_by("case_number")
-        cases = [Case(case.case_number, case.mandatory_operation, case.optional_operation, case.id) for case in cases]
+        cases = [DTO_Case(case.case_number, case.mandatory_operation, case.optional_operation, case.id) for case in
+                 cases]
 
         # players var
         players = self._player_DB.filter(board=board)
-        players = [Player(name=player.name, on_the_case=player.on_case.case_number, id=player.id) for player in players]
+        players = [
+            DTO_Player(name=player.name, on_the_case=player.on_case.case_number if player.on_case is not None else 0,
+                       id=player.id) for player in
+            players]
 
-        board = Board(id=board.id, cases=cases, dices=dices, moving_dice=movement_dice, result_dice=result_dice,
-                      players=players)
+        board = DTO_Board(id=board.id, cases=cases, dices=dices, moving_dice=movement_dice, result_dice=result_dice,
+                          players=players)
         return board
 
     def get_all_game(self):
         boards = self._board_DB.all()
         return [self.get_game_by_id(board.id) for board in boards]
-    #
-    # def save_game(self, game: board.Board):
-    #     self._games[game.id] = copy.deepcopy(game)
+
+    def save_game(self, dto_board: DTO_Board):
+        db_board = DB_Board()
+        if dto_board.id != 0:
+            db_board.id = dto_board.id
+        db_board.save()
+
+        for dto_case in dto_board.cases:
+            _save_case(db_board, dto_case)
+
+        for dto_dice in dto_board.dices:
+            _save_dice(db_board, dto_dice)
+
+        _save_dice(db_board, dto_board.moving_dice, False, True)
+        _save_dice(db_board, dto_board.result_dice, True, False)
+
+        for dto_player in dto_board.players:
+            if dto_player.on_the_case == 0:
+                db_player = DB_Player(board=db_board, name=dto_player.name)
+            else:
+                player_in_case = self._case_DB.get(from_board=db_board, case_number=dto_player.on_the_case)
+                db_player = DB_Player(board=db_board, name=dto_player.name, on_case=player_in_case)
+
+            if dto_player.id != 0:
+                db_player.id = dto_player.id
+            db_player.save()
