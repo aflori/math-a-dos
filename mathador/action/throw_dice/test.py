12206a854023a95b9b84912b623b9bc79b77da3b
asmuro1 <f.aurelien@yahoo.fr>
2023-11-03 21:24:38 +0100

    added command throw moving dice

--- mathador/action/throw_dice/test.py
+++ mathador/action/throw_dice/test.py
@@ -5,6 +5,7 @@ import pytest
 from mathador.DTO.board import Board
 from mathador.DTO.dice import Dice
 from mathador.DTO.repository.game_repository_in_memory import GameRepositoryInMemory
+from .throw_enigm_dices import EnigmDiceThrowCommand
 from .throw_movement_dice import MoveDiceThrowCommand
 
 
@@ -32,3 +33,19 @@ def test_throw_movement_dice(number_thrown):
     game: Board = repository_test.get_game_by_id(id_game)
     dice = game.moving_dice
     assert dice.last_number_throw == number_thrown
+
+
+@pytest.mark.parametrize("number_thrown", [1, 3, 8])
+def test_throw_enigm_dice(number_thrown):
+    id_game = 0
+    repository_test.save_game(
+        Board(cases=[], dices=[Dice(8), Dice(7), Dice(3)], moving_dice=Dice(8), result_dice=Dice(3), players=[],
+              id=id_game))
+    command = EnigmDiceThrowCommand(repository_test)
+    command.random = MockRandom(number_thrown)
+    command.execute(id_game)  # default id for repo in memory
+
+    game: Board = repository_test.get_game_by_id(id_game)
+    for dice in game.dices:
+        assert dice.last_number_throw == number_thrown
+    assert game.result_dice.last_number_throw == (number_thrown-1)
