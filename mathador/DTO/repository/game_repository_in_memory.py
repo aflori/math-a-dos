3febd76cb460002046e91aa2a35ce1dd2f93d153
asmuro1 <f.aurelien@yahoo.fr>
2023-11-03 21:01:17 +0100

    added command throw moving dice

--- mathador/DTO/repository/game_repository_in_memory.py
+++ mathador/DTO/repository/game_repository_in_memory.py
@@ -8,11 +8,12 @@ class GameRepositoryInMemory:
     def __init__(self):
         self._games = dict()
 
-    def get_game_by_id(self, id: int):
-        return copy.deepcopy(self._games[id])
+    def get_game_by_id(self, id_game: int):
+        return copy.deepcopy(self._games[id_game])
 
-    def get_all_game(self) -> dict[int, board.Board]:
-        return copy.deepcopy(self._games)
+    def get_all_game(self) -> list[board.Board]:
+        board_list = self._games.values()
+        return [copy.deepcopy(game) for game in board_list]
 
     def save_game(self, game: board.Board):
         self._games[game.id] = copy.deepcopy(game)
