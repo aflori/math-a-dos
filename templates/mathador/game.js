fdb255dbb5fd03f8de17e0743231e959905c8c67
asmuro1 <f.aurelien@yahoo.fr>
2023-11-03 20:01:29 +0100

    initialize JS script variables

--- templates/mathador/game.js
+++ templates/mathador/game.js
@@ -1,7 +1,18 @@
-const player = {{ player | safe }}
-const game_id = {{ game.id }}
+const player = {{ player | safe }};
+const game_id = {{  game.id }};
+
+const page_tag = {
+    player_position_tag: null,
+    next_case_position: null
+}
+
+function initialize_global_tag() {
+    page_tag.player_position_tag = document.querySelector(".player_position");
+    page_tag.next_case_position = document.querySelector("ul.next_case_positions");
+}
 
 window.addEventListener("DOMContentLoaded", (event) => {
+        initialize_global_tag();
 
     }
 );
