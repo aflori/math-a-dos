32fb293b819a21f215473c928e98007b77df889b
asmuro1 <f.aurelien@yahoo.fr>
2023-11-03 22:20:04 +0100

    update structure of JS for launching our API request

--- templates/mathador/game.js
+++ templates/mathador/game.js
@@ -1,18 +1,43 @@
-const player = {{ player | safe }};
-const game_id = {{  game.id }};
-
+// const player = {{ player | safe }};
+// const game_id = {{  game.id }};
+let is_game_running = true;
 const page_tag = {
     player_position_tag: null,
     next_case_position: null
+};
+
+function send_start_turn_request(element) {
+
 }
 
-function initialize_global_tag() {
+function send_throw_dice_request(element) {
+
+}
+
+function create_tag() {
     page_tag.player_position_tag = document.querySelector(".player_position");
     page_tag.next_case_position = document.querySelector("ul.next_case_positions");
+    page_tag.button_start_turn = document.createElement("button");
+    page_tag.button_throw_dice = document.createElement("button");
 }
 
-window.addEventListener("DOMContentLoaded", (event) => {
-        initialize_global_tag();
+function initialize_tag() {
+    page_tag.button_start_turn.textContent = "Se déplacer";
+    page_tag.button_start_turn.addEventListener("click", send_start_turn_request);
 
+    page_tag.button_throw_dice.textContent = "lancer les dés";
+    page_tag.button_throw_dice.addEventListener("click", send_throw_dice_request)
+
+}
+
+function initialize_global_tag_var() {
+    create_tag();
+    initialize_tag();
+}
+
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
+        initialize_global_tag_var();
     }
 );
