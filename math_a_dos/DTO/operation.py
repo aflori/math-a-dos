7e027a946b637fe61facd5b6347c020213325a0f
aflori <f.aurelien@yahoo.fr>
2023-11-19 00:57:06 +0100

    added a conversion function to convert JSON object list to dataclass list

--- math_a_dos/DTO/operation.py
+++ math_a_dos/DTO/operation.py
@@ -17,3 +17,13 @@ class TotalOperation:
     start_number: list[int]
     awaited_result: int
     list_operation: list[SingleOperation]
+
+def convert_single_operation_json_to_single_list(json_operations):
+    dataclass_list = []
+    for operation in json_operations:
+        dataclass_list.append(
+            SingleOperation(
+                operation_done=operation["operation_done"], number_1=operation["number_1"],
+                number_2=operation["number_2"], result=operation["result"], available_number_after_operation=operation["available_number_after_operation"])
+        )
+    return dataclass_list
