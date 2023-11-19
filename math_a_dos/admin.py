5c6cf810f651f5f8962d02eae6886dccaa3226b5
aflori <f.aurelien@yahoo.fr>
2023-11-19 21:32:48 +0100

    added admin interfazce for debug

--- math_a_dos/admin.py
+++ math_a_dos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Board)
+admin.site.register(Case)
+admin.site.register(Dice)
+admin.site.register(Player)
